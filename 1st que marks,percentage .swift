f74e8da472d2ee2510d0829a5072f7ecf7f91bda
rajat chawda <95015476+rajat8355@users.noreply.github.com>
2022-01-06 20:08:57 +0530

    Create 1st que marks,percentage .swift

---
+++ 1st que marks,percentage .swift
@@ -0,0 +1,16 @@
+WAP for assiging the grade from A to D for the marks/percentage got by student (90-100:A; 80-89:B; 70-79:C; 60-69:D
+
+var percentage = 75
+
+switch percentage {
+case 90 ..< 100: 
+    print("pass with A grade")
+case (80 ..< 89): 
+    print("pass with B grade")
+case (70 ..< 79): 
+    print(" pass with C grade")
+case (60 ..< 69): 
+    print("pass with D grade")
+default:
+    print("Please enter a valid percentage") 
+}
